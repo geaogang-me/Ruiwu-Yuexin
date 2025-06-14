9c096280cd7a2d18e488b45f2864c735d7f5aab2
gag <17814573237@163.com>
2025-06-14 09:30:33 +0800

    修改个人信息bug修复

--- Ruiwu-Yuexin/src/main/java/com/gag/RuiwuYuexin/service/UserService.java
+++ Ruiwu-Yuexin/src/main/java/com/gag/RuiwuYuexin/service/UserService.java
@@ -2,10 +2,8 @@ package com.gag.RuiwuYuexin.service;
 
 import com.gag.RuiwuYuexin.entity.User;
 
-import java.util.List;
-
 public interface UserService {
-    public void insertUser(User user);
+    void insertUser(User user);
     User findByUserName(String username);
     User findById(Long userId);
     User validateUser(String username, String password);
